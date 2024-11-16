d1dfb6c06144b9c26c895b17bc6971fb9f380409
Devin Coleman <croilders48@gmail.com>
2024-11-15 21:09:38 -0800

    finished app

---
+++ main.js
@@ -0,0 +1,59 @@
+let h2 = document.querySelector('h2')
+let hunger = document.querySelector('#hunger')
+let happiness = document.querySelector('#happiness')
+
+
+
+let pet = {
+
+hunger: 0,
+happiness: 0,
+feed: function(){
+    if(this.hunger >= 100){
+        alert('Pet is full!')
+        hunger.innerText = `Hunger: ${pet.hunger}`
+    }else {
+        this.hunger += 5
+        hunger.innerText = `Hunger: ${pet.hunger}`
+    }
+},
+
+play: function(){
+    if(this.happiness >= 100){
+        alert('Pet is full!')
+        happiness.innerText = `Happiness: ${pet.happiness}`
+    }else {
+        this.happiness += 5
+        happiness.innerText = `Happiness: ${pet.happiness}`
+    }
+},
+status: function(){
+    document.querySelector("h4").innerText =  (`${pet.name} is ${pet.hunger} full and is ${pet.happiness} happy`)
+}
+
+}
+
+
+
+h2.innerText = pet.name;
+hunger.innerText = `Hunger: ${pet.hunger}`
+happiness.innerText = `Happiness: ${pet.happiness}`
+
+let feed = document.querySelector('#feed')
+let play = document.querySelector('#play')
+
+play.addEventListener('click', () => pet.play())
+feed.addEventListener('click', () => pet.feed())
+
+let statuss = document.querySelector('#status')
+statuss.addEventListener('click', () => pet.status() )
+
+
+
+
+
+
+
+
+
+
